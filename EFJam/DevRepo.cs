b5e5bcd868c8db8059ea58a7ab5eb59dc54cb0da
Isaac Tumuti <geoftums@gmail.com>
2018-09-30 08:51:50 +0400

    Add project files.

---
+++ EFJam/DevRepo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace OreoJam
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T">Represents Name of The Table you want to query</typeparam>
+    /// <typeparam name="TEntity">Represents Name of The Database.</typeparam>
+    public class DevRepo<TEntity> where TEntity : DbContext, new()
+    {
+        #region Properties
+        private readonly TEntity db;
+        #endregion
+
+        #region Constructor
+        public DevRepo(TEntity _db)
+        {
+            db = _db;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Returns a List Of Items
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="predicate">Lambda expression i.e. x=>x.Id==1 or x=>x.Name==""</param>
+        /// <returns></returns>
+        public List<T> GetItems<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return db.Set<T>().Where(predicate).ToList();     
+        }
+        /// <summary>
+        /// Returns a single Item
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="predicate">Lambda expression i.e. x=>x.Id==1 or x=>x.Name==""</param>
+        /// <returns></returns>
+        public T GetItem<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return db.Set<T>().FirstOrDefault(predicate);      
+        }
+        /// <summary>
+        /// Adds many items.Batch Operation.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public void AddItems<T>(List<T> data) where T : class
+        {
+            db.Set<T>().AddRange(data);
+            db.SaveChanges();
+        }
+        /// <summary>
+        /// Adds a single item
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public void AddItem<T>(T data) where T : class
+        {
+           db.Set<T>().Add(data);
+           db.SaveChanges();
+        }
+        public void DeleteItem<T>(T data) where T : class
+        {
+             db.Entry(data).State = EntityState.Deleted;
+             db.SaveChanges();
+        }
+        public void UpdateItem<T>(T data) where T : class
+        {
+           db.Entry(data).State = EntityState.Modified;
+           db.SaveChanges();
+        }
+        #endregion
+    }
+}
---
+++ EFJam/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OreoJam")]
+[assembly: AssemblyDescription("A Lightweight .NET Package for CRUD(Create,Read,Update,Delete) methods against a DbContext Database.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OreoJam")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea034cc6-54b7-469d-87aa-de7e7b98b220")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
