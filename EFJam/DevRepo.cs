52d1d788d02ac8cbddbe182e7fcf617fb965af1b
Isaac Tumuti <geoftums@gmail.com>
2018-12-31 16:26:40 +0400

    Added async Methods

--- EFJam/DevRepo.cs
+++ EFJam/DevRepo.cs
@@ -30,18 +30,31 @@ namespace OreoJam
         {
             return db.Set<T>().Where(predicate).ToList();     
         }
-        public IQueryable<T> GetItemsIqueryable<T>(Expression<Func<T, bool>> predicate) where T : class
+        public async Task<List<T>> Get_Items<T>(Expression<Func<T, bool>> predicate) where T : class
         {
-            return db.Set<T>().Where(predicate);
+            return await db.Set<T>().Where(predicate).ToListAsync();
+        }
+        public IQueryable<T> Query<T>() where T : class
+        {
+            return db.Set<T>();
         }
         public T GetItem<T>(Expression<Func<T, bool>> predicate) where T : class
         {
             return db.Set<T>().FirstOrDefault(predicate);      
         }
+        public async Task<T> Get_Item<T>(Expression<Func<T, bool>> predicate) where T : class
+        {
+            return await db.Set<T>().FirstOrDefaultAsync(predicate);
+        }
         public void UpdateItem<T>(T data) where T:class
         {
             db.Entry(data).State = EntityState.Modified;
             db.SaveChanges();
+        }
+        public async Task Update_Item<T>(T data) where T : class
+        {
+            db.Entry(data).State = EntityState.Modified;
+            await db.SaveChangesAsync();
         }
         public void UpdateItems<T>(List<T> data) where T : class
         {
@@ -51,11 +64,25 @@ namespace OreoJam
             }
             db.SaveChanges();
         }
+        public async Task Update_Items<T>(List<T> data) where T : class
+        {
+            foreach (var item in data)
+            {
+                db.Entry(item).State = EntityState.Modified;
+            }
+           await db.SaveChangesAsync();
+        }
         public void DeleteItem<T>(T data) where T : class
         {
             db.Entry(data).State = EntityState.Deleted;
             db.SaveChanges();
         }
+        public async Task Delete_Item<T>(T data) where T : class
+        {
+            db.Entry(data).State = EntityState.Deleted;
+            await db.SaveChangesAsync();
+        }
+
         public void DeleteItems<T>(List<T> data) where T : class
         {
             foreach (var item in data)
@@ -64,15 +91,38 @@ namespace OreoJam
             }
             db.SaveChanges();
         }
-        public void AddItem<T>(T data) where T : class
+        public async Task Delete_Items<T>(List<T> data) where T : class
         {
-            db.Entry(data).State = EntityState.Deleted;
-            db.SaveChanges();
+            foreach (var item in data)
+            {
+                db.Entry(item).State = EntityState.Deleted;
+            }
+          await  db.SaveChangesAsync();
         }
+        public T AddItem<T>(T data) where T : class
+        {
+            db.Entry(data).State = EntityState.Added;
+            db.SaveChanges();
+            return db.Set<T>().LastOrDefault();
+        }
+
+        public async Task<T> Add_Item<T>(T data) where T : class
+        {
+            db.Entry(data).State = EntityState.Added;
+            await db.SaveChangesAsync();
+            return  db.Set<T>().LastOrDefault();
+        }
+
         public void AddItems<T>(List<T> data) where T : class
         {
             db.Set<T>().AddRange(data);
             db.SaveChanges();
+        }
+
+        public async Task Add_Items<T>(List<T> data) where T : class
+        {
+            db.Set<T>().AddRange(data);
+            await db.SaveChangesAsync();
         }
         #endregion
     }
